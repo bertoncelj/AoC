6b895dfd77f04fc60ed2578ec1f2ae81bf79fb13
tine <tine.bertoncelj@gmail.com>
2021-12-06 22:47:07 +0100

    typos

--- d1.py
+++ d1.py
@@ -3,9 +3,9 @@ import sys
 sys.setrecursionlimit(2300)
 
 
-def find_end_deepth(deeps_log):
+def find_end_depth(deep_log):
     return sum(list(map(lambda a,b: 1 if a < b else 0, 
-                    deeps_log[0:], deeps_log[1:])))
+                    deep_log[0:], deep_log[1:])))
 
 def main():
 #load file
@@ -21,13 +21,11 @@ def main():
                             deeps[0:], deeps[1:],deeps[2:]))
 
 
-    max_sigle_deepths = find_end_deepth(deeps)
+    max_sigle_depth = find_end_depth(deeps)
+    max_sum_triplets_depth = find_end_depth(sum_three_together)
 
-
-    max_sum_triplets_deepths = find_end_deepth(sum_three_together)
-
-    print(max_sigle_deepths)
-    print(max_sum_triplets_deepths )
+    print(max_sigle_depth)
+    print(max_sum_triplets_depth)
 
 
 if __name__ == "__main__":
