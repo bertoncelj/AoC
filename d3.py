ea6e696ba9f67430941724cd145dc4e9a3a972dd
tine <tine.bertoncelj@gmail.com>
2021-12-08 00:20:17 +0100

    day 3 task 2 done

--- d3.py
+++ d3.py
@@ -3,6 +3,33 @@ import numpy as np
 def bin_to_int(list_bin):
     return int("".join(list(map(str, list_bin))),2)
 
+def oxygen(bin, lvl):
+    if len(bin) == 1:
+        return bin
+
+    a = np.array(bin).transpose()
+    sum_rows = np.sum(a,axis=1)
+    gamma = list(map(lambda x: 1 if x >= len(bin)/2 else 0, list(sum_rows)))
+    new_bin = []
+    for b in bin:
+        if b[lvl] ==  gamma[lvl]:
+            new_bin.append(b)
+    return oxygen(new_bin, lvl+1)
+
+def co2(bin, lvl):
+    if len(bin) == 1:
+        return bin
+
+    a = np.array(bin).transpose()
+    sum_rows = np.sum(a,axis=1)
+    gamma = list(map(lambda x: 1 if x < len(bin)/2 else 0, list(sum_rows)))
+    new_bin = []
+    for b in bin:
+        if b[lvl] ==  gamma[lvl]:
+            new_bin.append(b)
+    return co2(new_bin, lvl+1)
+    
+
 file = open("binary_report.txt", "r")
 lines = file.readlines()
 
@@ -19,15 +46,23 @@ print(a.shape)
 sum_rows = np.sum(a,axis=1)
 gamma = list(map(lambda x: 1 if x > 500 else 0, list(sum_rows)))
 print("gamma :",  gamma)
-
 epsilon = list(map(lambda x: 0 if x == 1 else 1, list(gamma)))
 print("epsilon", epsilon)
 
 #task part one
-#display gamma*epsion
 num_gamma =   bin_to_int(gamma)
 num_epsilon = bin_to_int(epsilon)
 print("mul gamma*epsion = ", num_gamma*num_epsilon)
+
+#part two
+oxy = oxygen(bin, 0)
+co = co2(bin, 0)
+
+num_oxy =  bin_to_int(oxy[0])
+num_co =   bin_to_int(co[0])
+print("mul oxygen*co2 = ", num_co*num_oxy)
+
+
 
 
 
