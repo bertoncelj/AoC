42650b035e4ee0cca7486c1800861bc9ff21dfb0
berto <tine>
2022-12-21 17:43:36 +0100

    day2 short example works

--- 2022/d21.py
+++ 2022/d21.py
@@ -1,4 +1,17 @@
-input = """
+import sympy
+
+
+monkey = {}
+
+def read_input(input_file):
+    file = open(input_file, "r")
+    text = file.readlines()
+    text = [a.rstrip('\n') for a in text]
+    return text
+
+def main():
+    # buffer = read_input("file21.txt")
+    input = """
 root: pppw + sjmn\n
 dbpl: 5\n
 cczh: sllz + lgvd\n
@@ -15,31 +28,52 @@ lgvd: ljgn * ptdq\n
 drzm: hmdt - zczc\n
 hmdt: 32\n
 """
-monkey = {}
+    input = input.split("\n")
+    buffer = [a for a in input if a]
+    print(buffer)
+        
 
-def read_input(input_file):
-    file = open(input_file, "r")
-    text = file.readlines()
-    text = [a.rstrip('\n') for a in text]
-    return text
 
-def main():
-    buffer = read_input("file21.txt")
+    x = sympy.Symbol('x')
+    f = 3 + x
+    f = f - 3 + x + x + x - 2
+    print(f)
+    
+
+
+    xx = sympy.Eq(f,0)
+    print(sympy.solve(xx))
+
+    monkey["humn"] = sympy.Symbol("x")
 
     for call in buffer:
+        print(call)
+        print(monkey)
         name, expr = call.split(": ")
+        if name == "humn":
+            continue
         if expr.isnumeric():
             monkey[name] = int(expr)
+            print(sympy.solve(xx))
         else:
             left, op, right = expr.split()
             if left in monkey and right in monkey:
-                monkey[name] = eval(str(monkey[left]) + op + str(monkey[right]))
-                pass
+                if name == "root":
+                    xx = sympy.Eq(monkey[left],monkey[right])
+                    print(sympy.solve(xx))
+                    break
+
+                if op == "+":
+                    monkey[name] = monkey[left] + monkey[right]
+                elif op == "-":
+                    monkey[name] = monkey[left] - monkey[right]
+                elif op == "*":
+                    monkey[name] = monkey[left] * monkey[right]
+                elif op == "/":
+                    monkey[name] = monkey[left] / monkey[right]
             else:
                 buffer.append(call)
          
-    print(monkey["root"])
 
 if "__main__" == __name__:
     main()
-
