21d09d8d91cda52df8ee33010b6e97f662a350f9
berto <tine>
2022-12-21 17:45:54 +0100

    day2 done

--- 2022/d21.py
+++ 2022/d21.py
@@ -1,6 +1,5 @@
 import sympy
 
-
 monkey = {}
 
 def read_input(input_file):
@@ -10,51 +9,35 @@ def read_input(input_file):
     return text
 
 def main():
-    # buffer = read_input("file21.txt")
-    input = """
-root: pppw + sjmn\n
-dbpl: 5\n
-cczh: sllz + lgvd\n
-zczc: 2\n
-ptdq: humn - dvpt\n
-dvpt: 3\n
-lfqf: 4\n
-humn: 5\n
-ljgn: 2\n
-sjmn: drzm * dbpl\n
-sllz: 4\n
-pppw: cczh / lfqf\n
-lgvd: ljgn * ptdq\n
-drzm: hmdt - zczc\n
-hmdt: 32\n
-"""
-    input = input.split("\n")
-    buffer = [a for a in input if a]
-    print(buffer)
-        
-
-
-    x = sympy.Symbol('x')
-    f = 3 + x
-    f = f - 3 + x + x + x - 2
-    print(f)
+    buffer = read_input("file21.txt")
     
-
-
-    xx = sympy.Eq(f,0)
-    print(sympy.solve(xx))
+    # input = """
+# root: pppw + sjmn\n
+# dbpl: 5\n
+# cczh: sllz + lgvd\n
+# zczc: 2\n
+# ptdq: humn - dvpt\n
+# dvpt: 3\n
+# lfqf: 4\n
+# humn: 5\n
+# ljgn: 2\n
+# sjmn: drzm * dbpl\n
+# sllz: 4\n
+# pppw: cczh / lfqf\n
+# lgvd: ljgn * ptdq\n
+# drzm: hmdt - zczc\n
+# hmdt: 32\n
+# """
+        
 
     monkey["humn"] = sympy.Symbol("x")
 
     for call in buffer:
-        print(call)
-        print(monkey)
         name, expr = call.split(": ")
         if name == "humn":
             continue
         if expr.isnumeric():
             monkey[name] = int(expr)
-            print(sympy.solve(xx))
         else:
             left, op, right = expr.split()
             if left in monkey and right in monkey:
