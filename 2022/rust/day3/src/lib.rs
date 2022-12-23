3104718cb7a76ea8e71dbb488d9a546fd1f7aec1
berto <tine>
2022-12-24 00:25:05 +0100

    d3 rust part 2 done

--- 2022/rust/day3/src/lib.rs
+++ 2022/rust/day3/src/lib.rs
@@ -1,4 +1,6 @@
 use std::collections::HashSet;
+use std::collections::hash_map::RandomState;
+
 fn get_num(input: char) -> u32 {
     let mut alphabet = ('a'..='z').into_iter().collect::<Vec<char>>();
     alphabet.append(&mut ('A'..='Z').into_iter().collect::<Vec<char>>());
@@ -6,14 +8,10 @@ fn get_num(input: char) -> u32 {
     (index + 1)  as u32
 }
 
-
 pub fn process_part1(input: &str) -> String {
     let mut sum_score = 0;
     for line in  input.split('\n'){
-        let n = line.len();
-        assert_eq!(n%2, 0);
-        let bag1 = &line[..n/2];
-        let bag2 = &line[n/2..];
+        let (bag1,bag2) = line.split_at(line.len()/2);
         let s1:HashSet<char> = HashSet::from_iter(bag1.chars());
         let s2:HashSet<char> = HashSet::from_iter(bag2.chars());
         for c in s1.intersection(&s2) {
@@ -24,7 +22,22 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    "result".to_string()
+    let mut tot = 0;
+    let mut sets: Vec<HashSet<u8, RandomState>> = Vec::new();
+
+    for line in input.lines(){
+        sets.push(HashSet::from_iter(line.bytes()));
+            
+    }
+    
+    for s in sets.chunks(3){
+        let (a,b,c) = (&s[0],&s[1],&s[2]);
+        let ab:HashSet<u8, RandomState> = 
+            HashSet::from_iter(a.intersection(b).cloned());
+        let abc = ab.intersection(c).next().unwrap();
+        tot += get_num(*abc as char);
+    }
+    tot.to_string()
 }
 
 
