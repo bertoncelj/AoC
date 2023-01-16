d01b2dc8d1481b879168032e36ee7eb93a550d77
tine <tine.bertoncelj@gmail.com>
2023-01-16 12:05:02 +0100

    part 1 done

--- 2022/d10.py
+++ 2022/d10.py
@@ -4,31 +4,31 @@ addx -5"""
 
 cycle = 0
 x = 1
-si = False
 
 # open the data file
 file = open("file10.txt")
 with open("file10.txt") as f:
     data = list(map(str.rstrip,f))
 
-print(data)
-check = list(range(20,220,20))
+# check intervales
+check = list(range(20,1000,40))
+
+#insert noops before each addx
 xx = [["noop"] if a == "noop" else ["noop",a] for a in data]
 test = [item for sublist in xx for item in sublist]
 
+signal = []
 for line in test:
     cycle += 1
+
+    if check[0] == cycle:
+        c = check.pop(0)
+        signal.append(c*x)
+
     if "noop" == line:
         pass
     else:
         _, num = line.split()
         x += int(num)
-    if check[0] == cycle:
-        c = check.pop(0)
-        print(c)
-        print(x)
-        print(c*x)
 
-print("x:", x) 
-print("cycle:", cycle) 
-
+print(sum(signal))
